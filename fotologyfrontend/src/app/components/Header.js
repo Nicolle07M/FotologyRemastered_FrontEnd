f6f85c2525597333a2c9ef3be04e69a32e7504dd
Nicolle <marqueznicolle421@gmail.com>
2025-04-24 10:52:13 -0500

    feat(Frontend): add responsive landingPage with header

--- fotologyfrontend/src/app/components/Header.js
+++ fotologyfrontend/src/app/components/Header.js
@@ -1,17 +1,28 @@
-import styles from '../styles/Header.module.css'
-import Link from 'next/link'
+'use client';
+import React, { useState } from 'react';
+import styles from '../styles/Header.module.css';
+import Link from 'next/link';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={styles.header}>
       <h2 className={styles.logo}>Fotology</h2>
-      <nav className={styles.nav}>
+      <button className={styles.hamburger} onClick={toggleMenu}>
+        ☰
+      </button>
+      <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}>
         <Link href="/" className={styles.link}>Inicio</Link>
         <Link href="/Register" className={styles.link}>Registrarse</Link>
         <Link href="/Login" className={`${styles.link} ${styles.loginButton}`}>Iniciar sesión</Link>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
